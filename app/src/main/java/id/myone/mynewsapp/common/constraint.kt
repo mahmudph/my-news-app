efaa40ac1ae7ecf454b5c05c9b337d6e29ccef67
mahmud <mahmud120398@gmail.com>
2023-09-04 13:11:47 +0700

    refactor: update constraint

--- app/src/main/java/id/myone/mynewsapp/common/constraint.kt
+++ app/src/main/java/id/myone/mynewsapp/common/constraint.kt
@@ -12,9 +12,8 @@ const val sourceTable = "tbl_sources"
 const val sourceRemoteKeyTable = "tbl_source_remote_keys"
 
 
-//const val hostUrl = "https://newsapi.org/"
-const val hostUrl = "http://192.168.38.103:3000/"
-const val apiKey = "e14a04ae436240db841224a99dbdd24c"
+const val hostUrl = "https://newsapi.org/"
+const val apiKey = ""
 
 const val pageSize = 10
 const val language = "en"
